c225f79d8eb4e0eb32bb9d8008bc132697150315
Raden Arsil Kusumah Dinata <arsilkusumah1234@gmail.com>
2026-06-12 15:02:34 +0700

    Update: Optimasi rasio aspek galeri untuk mobile

--- resources/views/partials/galeri.blade.php
+++ resources/views/partials/galeri.blade.php
@@ -6,13 +6,13 @@
     </div>
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-4xl mx-auto">
-        <div class="rounded-xl overflow-hidden shadow-lg aspect-[4/3] sm:aspect-square md:aspect-[3/4] bg-stone-100 border border-white">
+        <div class="rounded-xl overflow-hidden shadow-lg aspect-[3/3] sm:aspect-square md:aspect-[3/4] md:aspect-[16/9] bg-stone-100 border border-white">
             <img src="{{ asset('assets/image/E&H_5.jpeg') }}" alt="Gallery" class="w-full h-full object-cover object-center hover:scale-105 transition duration-700">
         </div>
-        <div class="rounded-xl overflow-hidden shadow-lg aspect-[4/3] sm:aspect-square md:aspect-[3/4] sm:row-span-2 sm:h-full bg-stone-100 border border-white">
+        <div class="rounded-xl overflow-hidden shadow-lg aspect-[3/3] sm:aspect-square md:aspect-[3/4] sm:row-span-2 sm:h-full bg-stone-100 border border-white">
             <img src="{{ asset('assets/image/E&H_6.jpeg') }}" alt="Gallery" class="w-full h-full object-cover object-center hover:scale-105 transition duration-700">
         </div>
-        <div class="rounded-xl overflow-hidden shadow-lg aspect-[4/3] sm:aspect-square md:aspect-[3/4] bg-stone-100 border border-white">
+        <div class="rounded-xl overflow-hidden shadow-lg aspect-[3/3] sm:aspect-square md:aspect-[3/4] bg-stone-100 border border-white">
             <img src="{{ asset('assets/image/E&H_7.jpeg') }}" alt="Gallery" class="w-full h-full object-cover object-center hover:scale-105 transition duration-700">
         </div>
     </div>
